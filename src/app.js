594eea05e305bb85556c189109066d7a214fe470
Joseguera <joseguera@uclaextension.edu>
2021-01-10 22:10:54 -0800

    first server deploy

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const errorHandler = require('./error-handler');
 const coursesRouter = require('../courses/courses-router');
+const questionsRouter = require('../questions/questions-router');
 const authRouter = require('./auth/auth-router')
 const usersRouter = require('./users/users-router')
 
@@ -15,14 +16,15 @@ const app = express();
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
-
 app.use(morgan(morganOption));
+
 app.use(helmet());
 app.use(cors());
 
 app.use(express.json());
 
 app.use('/api/courses', coursesRouter);
+app.use('/api/questions', questionsRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
 

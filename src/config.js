69d708dc3e35caa0d8550be89c06da184dc27641
Joseguera <joseguera@uclaextension.edu>
2021-01-12 05:43:03 -0800

    testing router

--- src/config.js
+++ src/config.js
@@ -2,14 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://localhost/coursegrader',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://localhost/coursegrader?ssl=true',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://localhost/coursegrader',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-    port: 5432,
-    ssl: true,
-    dialect: 'postgres',
-    dialectOptions: {
-      "ssl": {"require":true }
-    }
-
 }
